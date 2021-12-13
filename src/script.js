cda7a8410d340e34bec1426dab728ec2c3ab4080
ashleyjanee <jrn2013@gmail.com>
2021-12-12 20:30:45 -0600

    Added api integration to get 6 day forecast

--- src/script.js
+++ src/script.js
@@ -24,27 +24,46 @@ function search(event) {
 let form = document.querySelector("#search-input");
 form.addEventListener("submit", search);
 
-function displayForecast() {
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  let days = ["Saturday", "Sunday", "Monday"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
           <div class="col-2">
-            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-date">${formatDay(
+              forecastDay.dt
+            )}</div>
             <img
-              src="https://ssl.gstatic.com/onebox/weather/48/rain.png"
+              src="http://openweathermap.org/img/wn/${
+                forecastDay.weather[0].icon
+              }@2x.png"
               alt=""
               width="36"
             />
             <div class="weather-forecast-temp">
-              <span class="weather-forecast-temp-max">22°</span>
-              <span class="weather-forecast-temp-min">-5°</span>
+              <span class="weather-forecast-temp-max">${Math.round(
+                forecastDay.temp.max
+              )}°</span>
+              <span class="weather-forecast-temp-min">${Math.round(
+                forecastDay.temp.min
+              )}°</span>
             </div>
           </div>
         `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
